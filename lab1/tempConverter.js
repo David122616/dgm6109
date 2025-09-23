99446b95ff9f6de8064d14d61ed687f9925dad94
David122616 <1103000385@qq.com>
2025-09-23 16:40:21 -0700

    Delete lab1 directory

--- lab1/tempConverter.js
+++
@@ -1,6 +0,0 @@
-let fahrenheit = 56;//Beijing Jan 2025 average temp in 57°F： https://zh.weatherspark.com/h/y/131055/2025/2025%E5%B9%B4-in-%E4%B8%AD%E5%9B%BD%E3%80%81%E5%8C%97%E4%BA%AC%E5%B8%82%E6%9C%9F%E9%97%B4%E7%9A%84%E5%8E%86%E5%8F%B2%E5%A4%A9%E6%B0%94#google_vignette
-let celsius= (fahrenheit-32) *5/9;
-let kelvin=celsius+273.15;
-console.log("Temperature (fahrenheit): "+fahrenheit);
-console.log("Temperature (celsius): "+celsius);
-console.log("Temperature (kelvin): "+kelvin);
